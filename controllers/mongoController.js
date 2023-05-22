31c8cc8f59884f06d5787b95c26f16b901b230f8
lindblomdavid <daavidlindblom@gmail.com>
2023-05-22 18:28:34 +0200

    added user logging

--- controllers/mongoController.js
+++ controllers/mongoController.js
@@ -10,22 +10,41 @@ const client = new MongoClient(uri, {
   },
 });
 
-const saveChatHistory = async (chatHistory) => {
+const saveChatHistory = async (message, auth0Id, email) => {
   try {
-    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-    const collection = client.db('mimerchat').collection('chats');
 
-    // Add chatId and timestamp
-    chatHistory.chatId = new ObjectId();
-    chatHistory.timestamp = new Date();
+    const usersCollection = client.db('mimerchat').collection('users');
+    const chatsCollection = client.db('mimerchat').collection('chats');
 
-    await collection.insertOne(chatHistory);
+    // Check if the user already exists
+    let user = await usersCollection.findOne({ auth0Id });
+
+    if (!user) {
+      // User doesn't exist, create a new user
+      const newUser = {
+        auth0Id,
+        userId: new ObjectId(),
+        email,
+      };
+      await usersCollection.insertOne(newUser);
+      console.log('User created successfully!');
+      user = newUser;
+    }
+
+    // Add userId, chatId and timestamp to chat history
+    const chatHistory = {
+      userId: user.userId,
+      chatId: new ObjectId(),
+      timestamp: new Date(),
+      history: message.history,
+    };
+
+    await chatsCollection.insertOne(chatHistory);
     console.log('Chat history saved successfully!');
   } catch (error) {
     console.error('Error occurred while saving chat history:', error);
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 };
--- routes/saveToMongo.js
+++ routes/saveToMongo.js
@@ -4,7 +4,8 @@ const mongoController = require('../controllers/mongoController');
 
 router.post('/', async (req, res) => {
   try {
-    await mongoController.saveChatHistory(req.body);
+    const { auth0Id, email, message } = req.body.chatHistory;
+    await mongoController.saveChatHistory(message, auth0Id, email);
     res.status(200).send('Chat history saved successfully');
   } catch (error) {
     console.error('Error occurred while saving chat history:', error);
