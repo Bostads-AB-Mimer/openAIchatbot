671910b795f9b972d3a6148e16b8bc5fcab061d4
lindblomdavid <daavidlindblom@gmail.com>
2023-05-23 09:44:52 +0200

    adjusted mongodb connection

--- controllers/mongoController.js
+++ controllers/mongoController.js
@@ -1,3 +1,15 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = process.env.MONGO_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 const saveChatHistory = async (message, auth0Id, email) => {
   try {
     await client.connect();
@@ -41,3 +53,5 @@ const saveChatHistory = async (message, auth0Id, email) => {
     await client.close();
   }
 };
+
+module.exports = { saveChatHistory };
